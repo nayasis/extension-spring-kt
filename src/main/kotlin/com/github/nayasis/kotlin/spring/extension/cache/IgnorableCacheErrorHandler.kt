bf2538865c4f6630f8f0d7ee7c1edf22e1d5b9a3
nayasis <nayasis@gmail.com>
2022-10-05 17:53:41 +0900

    down log level to warn in getting cache

--- src/main/kotlin/com/github/nayasis/kotlin/spring/extension/cache/IgnorableCacheErrorHandler.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/spring/extension/cache/IgnorableCacheErrorHandler.kt
@@ -7,7 +7,7 @@ import org.springframework.cache.interceptor.CacheErrorHandler
 private val log = KotlinLogging.logger{}
 
 class IgnorableCacheErrorHandler: CacheErrorHandler {
-    override fun handleCacheGetError(e: RuntimeException, cache: Cache, o: Any) = log.error(e.message, e)
+    override fun handleCacheGetError(e: RuntimeException, cache: Cache, o: Any) = log.warn(e.message, e)
     override fun handleCachePutError(e: RuntimeException, cache: Cache, o: Any, o1: Any?) = log.error(e.message, e)
     override fun handleCacheEvictError(e: RuntimeException, cache: Cache, o: Any) = log.error(e.message, e)
     override fun handleCacheClearError(e: RuntimeException, cache: Cache) = log.error(e.message, e)
